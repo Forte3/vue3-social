47ce8c5be5e9ad4d0203e21c4e13f541f4fc606b
Forte <lianghaizhao@icloud.com>
2022-09-22 23:05:46 +0800

    modify ProfilePage

---
+++ social/src/pages/ProfilePage.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <div class="profileContainer">
+      <TheAvatar :width="186" :height="186" />
+      <div class="profile">
+        <p class="name">
+          <span>张XX</span>
+          <router-link to="/profile/edit">编辑个人资料</router-link>
+        </p>
+        <p class="handle">@yufeng_zhang</p>
+        <div class="description">
+          <pre>
+📷90后摄影师。
+🏫毕业于中央艺术学院
+🎹钢琴8级
+🎨热爱生活，从生活中发现艺术
+          </pre>
+        </div>
+        <p class="website">https://yufeng.com</p>
+      </div>
+    </div>
+    <div class="tabs">
+      <div class="tab active">
+        <TheIcon icon="posts" />
+        <p>我的</p>
+      </div>
+      <div class="tab">
+        <TheIcon icon="like" />
+        <p>赞过</p>
+      </div>
+      <div class="tab">
+        <TheIcon icon="favorite" />
+        <p>收藏</p>
+      </div>
+    </div>
+    <div class="tabContent">
+      <p>162 篇帖子</p>
+      <div class="posts">
+        <img src="" class="postImage" v-for="n in 9" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TheAvatar from '../components/TheAvatar.vue';
+import TheIcon from '../components/TheIcon.vue';
+
+</script>
+
+<style scoped>
+.profileContainer {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 10vw;
+}
+
+.avatar {
+  justify-self: end;
+}
+
+.profile .name {
+  display: flex;
+  align-items: center;
+}
+
+.profile .name>span {
+  font-size: 26px;
+}
+
+.profile .name>a {
+  color: #1da0ff;
+  text-decoration: none;
+  margin-left: 26px;
+}
+
+.profile .handle {
+  margin-top: 4px;
+  color: #848484;
+}
+
+.profile .description {
+  margin-top: 26px;
+  margin-bottom: 22px;
+}
+
+.tabs {
+  display: grid;
+  grid-template-columns: repeat(3, 88px);
+  column-gap: 4vw;
+  justify-content: center;
+
+  margin-top: 7vmin;
+  margin-bottom: 20px;
+}
+
+.tab {
+  text-align: center;
+  padding: 12px 0;
+  cursor: pointer;
+}
+
+.tab>svg {
+  width: 32px;
+  height: 32px;
+  stroke: #8a9194;
+  fill: #8a9194;
+}
+
+.tab.active {
+  background: #f6f9fb;
+  border-radius: 18px;
+}
+
+.tab.active>svg {
+  stroke: #1787d9;
+  fill: #1787d9;
+}
+
+.tab.active>p {
+  color: #1787d9;
+}
+
+.tabContent>p {
+  text-align: center;
+  font-weight: 600;
+  margin-bottom: 32px;
+}
+
+.posts {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 40px;
+}
+
+.postImage {
+  width: 100%;
+  height: 321px;
+  background: #eee;
+  object-fit: cover;
+}
+</style>
---
+++ social/src/pages/SearchPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2 class="title">搜索结果: 植物</h2>
+    <PostList>
+      <PostItem v-for="n in 10" />
+    </PostList>
+    <!-- <PostDetails /> -->
+    <!-- <PostUpload /> -->
+  </div>
+</template>
+
+<script setup>
+import PostList from '../components/PostList.vue';
+import PostItem from '../components/PostItem.vue';
+
+</script>
+
+<style scoped>
+.title {
+  font-weight: 600;
+  font-size: 24px;
+  margin-bottom: 44px;
+}
+</style>
